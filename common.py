cb66b97576f1e961d23a44b1afc58d5efd283837
Nicolas Cabrera <nicocabrera.0212@gmail.com>
2021-05-26 14:58:39 -0300

    cambios minimos

--- common.py
+++ common.py
@@ -1,11 +1,13 @@
 import yaml
 
+
 __config = None
 
 
 def config():
-	global __config
-	if not __config:
-		with open('config.yaml', mode='r') as f:		#Para realizar nuestra “conversion” se hace uso de la
-			__config = yaml.safe_load(f)				#funcionyaml.safe_load(f) que recibe el archivo 
-	return __config										#como parametro y retorna un diccionario.
+    global __config
+    if not __config:
+        with open('config.yaml', mode='r') as f:
+            __config = yaml.load(f)
+
+    return __config
--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-import datetime
-import csv
 import argparse
+import csv
+import datetime
 import logging
-from os import write                              # loggin permite  enviar mensajes por consola de manera automatica
-logging.basicConfig(level=logging.INFO)     # asignandole un nivel (level) de importancia a cada tipo de mensaje
-import re								
+logging.basicConfig(level=logging.INFO)
+import re
 
-from requests.exceptions import HTTPError 
+from requests.exceptions import HTTPError
 from urllib3.exceptions import MaxRetryError
-				
+
 import news_page_objects as news
 from common import config
 
 
-logger = logging.getLogger(__name__) 		#retorna una instancia que nos servira para indicar que los mensajes
-											#estan siendo enviados desde este module name en particular.
-is_well_formed_link = re.compile(r'^https?://.+/.+$')
-is_root_path = re.compile(r'^/.+$')
+logger = logging.getLogger(__name__)
+is_well_formed_link = re.compile(r'^https?://.+/.+$') # https://example.com/hello
+is_root_path = re.compile(r'^/.+$') # /some-text
 
 
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
 
     logging.info('Beginning scraper for {}'.format(host))
-    logging.info('Finding links in homepage...')
     homepage = news.HomePage(news_site_uid, host)
 
     articles = []
@@ -31,12 +28,27 @@ def _news_scraper(news_site_uid):
         article = _fetch_article(news_site_uid, host, link)
 
         if article:
-            logger.info('Article fetched')
+            logger.info('Article fetched!!')
             articles.append(article)
-            print(article.title)
-            
 
     _save_articles(news_site_uid, articles)
+
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(
+        news_site_uid=news_site_uid,
+        datetime=now)
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+    
+    with open(out_file_name, mode='w+', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
+
 
 def _fetch_article(news_site_uid, host, link):
     logger.info('Start fetching article at {}'.format(link))
@@ -47,24 +59,13 @@ def _fetch_article(news_site_uid, host, link):
     except (HTTPError, MaxRetryError) as e:
         logger.warning('Error while fechting the article', exc_info=False)
 
+
     if article and not article.body:
-        logger.warning('Invalid Article. There is no body')
+        logger.warning('Invalid article. There is no body')
         return None
-    
+
     return article
 
-def _save_articles(news_site_uid, articles):
-    now = datetime.datetime.now()
-    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
-    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(news_site_uid=news_site_uid, datetime=now.strftime('%Y_%m_%d'))
-
-    with open(out_file_name, mode='w+', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(csv_headers)
-
-        for article in articles:
-            row = [str(getattr(article, prop)) for prop in csv_headers]
-            writer.writerow(row)
 
 def _build_link(host, link):
     if is_well_formed_link.match(link):
@@ -73,7 +74,6 @@ def _build_link(host, link):
         return '{}{}'.format(host, link)
     else:
         return '{host}/{uri}'.format(host=host, uri=link)
-
 
 
 if __name__ == '__main__':
